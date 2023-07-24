086bd1e533c07803f7e35fe4830dbb8d968449cd
Roguska <agnieszka.roguska@capgemini.com>
2023-07-24 16:09:25 +0200

    second approach

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-board_size = 9
+board_size = 4
+
 class Cell:
     """
-    The smallest part of a sudoku board - the class that stores the data of a single cell
+    The smallest part of a sudoku board - the class that stores the data for a single cell
     """
     possible_values = set()
     for i in range(board_size):
@@ -11,7 +12,7 @@ class Cell:
         self.value = None
         self.row_number = None
         self.column_number = None
-    def set_value(self, value): #set cell to a given value(method input) and remove this element from possible values for this column/row
+    def set_value(self, value): #sets cell to a given value(method's input)
         if value in self.possible_values:
             self.value = value
             self.possible_values = {}
@@ -20,10 +21,10 @@ class Row:
     def __init__(self): #creates N Cell class instances (N = board size)
         self.row_cells = [Cell() for cell in range(board_size)]
     def get_possible_values(self): #gets the possible values for this row
-        possible_values = {}
-        for cell in range(self.row_cells):
-            if self.row_cells[cell].value in possible_values:
-                possible_values = possible_values.union(self.row_cells[cell].possible_values)
+        possible_values = set()
+        for cell in range(len(self.row_cells)):
+            possible_values = possible_values.union(self.row_cells[cell].possible_values)
+        return possible_values
     def print_row(self):
         for cell in self.row_cells:
             print(cell.value, end = "\t")
@@ -31,11 +32,7 @@ class Row:
 
 class Column:
     def __init__(self):
-        column_cells = [None] * board_size
-        for i in range(board_size):
-            for row in Board.rows:
-                column_cells[i] += row.row_cells[i]
-        self.column_cells = [Row.row_cells[i] for i in range(Row.row_cells)]
+        self.column_cells = [None] * board_size
     def get_possible_values(self): #gets the possible values for this column
         possible_values = {}
         for cell in range(self.row_cells):
@@ -48,25 +45,42 @@ class Column:
 
 class Board:
     def __init__(self):
-        self.rows = [Row() for row in range(board_size)]
-        self.columns = [self.rows[i] for i in range(board_size)]
+        self.rows = [Row() for row_number in range(board_size)]
+        self.columns = [Column() for column_number in range(board_size)]
+        for column_number in range(board_size):
+            temp = []
+            for row_number in range(board_size):
+               temp.append(self.rows[row_number].row_cells[column_number])
+            self.columns[column_number].column_cells = temp[:]
         for row_number in range(len(self.rows)):
             for cell in self.rows[row_number].row_cells:
                 cell.row_number = row_number
-        """for column_number in range(len(self.columns)):
+        for column_number in range(len(self.columns)):
             for cell in self.columns[column_number].column_cells:
-                cell.column_number = column_number"""
+                cell = column_number
+    def set_cell_value(self, row_number, column_number, value):
+        (self.rows[row_number].row_cells[column_number]).set_value(value)
+        ################################################################# Do poprawy - possible values źle ustawiane podczas wpisywania wartosci do komorki
+        """for cell in self.rows[row_number].row_cells:
+            if value in cell.possible_values:
+                cell.possible_values.remove(value)"""
+        """for column in self.columns:
+            for cell in range(len(column)):
+                if value in column.column_cells[cell]"""
+        #################################################################
     def print_rows(self):
         for row in self.rows:
             row.print_row()
     def print_columns(self):
         for column in self.columns:
-            column.print_row()
+            column.print_column()
 
 board = Board()
-board.rows[1].row_cells[1].set_value(3)
-board.columns[3].column_cells[2].set_value(1)
-board.print_columns()
+board.set_cell_value(1,1,3)
+board.set_cell_value(1,2,1)
+board.set_cell_value(2,0,2)
 print("\n\n")
 board.print_rows()
-print(board.rows[1].row_cells[1].row_number, board.rows[1].row_cells[1].column_number)
+print(board.rows[1].row_cells[2].value, board.rows[1].row_cells[2].possible_values)
+print(board.rows[0].row_cells[1].value, board.rows[1].row_cells[1].possible_values)
+print(board.rows[3].get_possible_values())
