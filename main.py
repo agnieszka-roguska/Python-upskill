9ab9bfbd0d9afbe80da53b9cb2a17f1e0387493c
Roguska <agnieszka.roguska@capgemini.com>
2023-07-26 08:58:52 +0200

    25.07 version updated (possible values for columns - corrected)

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ class Board:
         self.columns = [Column() for column_number in range(self.board_size)]
         self.cells = [Cell(self.board_size) for cell in range(self.board_size**2)]
         for index, cell in enumerate(self.cells):
-            self.rows[index//self.board_size].append(cell)
+            self.rows[index // self.board_size].append(cell)
             self.columns[index % self.board_size].append(cell)
-
-    def set_cell_value(self, row_number, column_number, value):
-        (self.rows[row_number].row_cells[column_number]).set_value(value)
 
     def print_rows(self):
         for row in self.rows:
@@ -25,7 +22,7 @@ class Row():
     def __init__(self):
         self.row_cells = []
 
-    def get_possible_values_row(self): #gets the possible values for this row
+    def get_possible_values_row(self):
         possible_values = set()
         for cell in self.row_cells:
             possible_values = possible_values.union(cell.possible_values)
@@ -46,23 +43,19 @@ class Row():
 
 
 class Column():
-    column_number = None
     def __init__(self):
         self.column_cells = []
 
-    def get_possible_values_column(self): #gets the possible values for this column
-        possible_values = set()
-        for cell in range(len(self.column_cells)):
-            possible_values = possible_values.union(self.column_cells[cell].possible_values)
+    def get_possible_values_column(self):
+        possible_values_col = set()
+        for cell in self.column_cells:
+            possible_values_col = possible_values_col.union(cell.possible_values)
+        return possible_values_col
 
     def print_column(self):
         for cell in self.column_cells:
             print(cell.value, end = "\t")
         print("\n")
-
-    def set_cells_column_numbers(self):
-        for cell in self.column_cells:
-            cell.column_number = self.column_number
 
     def append(self, cell):
         self.column_cells.append(cell)
@@ -74,7 +67,7 @@ class Column():
 
 
 class Cell():
-    def __init__(self, value_space): #default cell properties
+    def __init__(self, value_space):
         self.possible_values = set(range(1, value_space + 1))
         self.value = None
         self.row = None
@@ -83,36 +76,27 @@ class Cell():
     def set_value(self, value):
         if value in self.possible_values:
             self.value = value
-            self.possible_values = {}
+            self.possible_values = set()
             self.row.erase(value)
+            self.column.erase(value)
+        else:
+            print("This cell already has value.")
     
     def erase(self, value):
-        self.possible_values.remove(value)
+        self.possible_values.discard(value)
 
 
 if __name__=="__main__":    
     board = Board()
-    print("\n\n")
+    board.cells[0].set_value(1)
+    board.cells[10].set_value(2)
+    board.cells[10].set_value(3)
     board.print_rows()
-    print("Row 0")
-    print("Value and possible_values",board.rows[0].row_cells[0].value, board.rows[0].row_cells[0].possible_values)
-    print("Value and possible_values", board.rows[0].row_cells[1].value, board.rows[0].row_cells[1].possible_values)
-    print("Value and possible_values", board.rows[0].row_cells[2].value, board.rows[0].row_cells[2].possible_values)
-    print("Value and possible_values", board.rows[0].row_cells[3].value, board.rows[0].row_cells[3].possible_values)
-    print("Row 1")
-    print("Value and possible_values",board.rows[1].row_cells[0].value, board.rows[1].row_cells[0].possible_values)
-    print("Value and possible_values", board.rows[1].row_cells[1].value, board.rows[1].row_cells[1].possible_values)
-    print("Value and possible_values", board.rows[1].row_cells[2].value, board.rows[1].row_cells[2].possible_values)
-    print("Value and possible_values", board.rows[1].row_cells[3].value, board.rows[1].row_cells[3].possible_values)
-    print("Row 2")
-    print("Value and possible_values",board.rows[2].row_cells[0].value, board.rows[2].row_cells[0].possible_values)
-    print("Value and possible_values", board.rows[2].row_cells[1].value, board.rows[2].row_cells[1].possible_values)
-    print("Value and possible_values", board.rows[2].row_cells[2].value, board.rows[2].row_cells[2].possible_values)
-    print("Value and possible_values", board.rows[2].row_cells[3].value, board.rows[2].row_cells[3].possible_values)
-    print("Row 3")
-    print("Value and possible_values",board.rows[3].row_cells[0].value, board.rows[3].row_cells[0].possible_values)
-    print("Value and possible_values", board.rows[3].row_cells[1].value, board.rows[3].row_cells[1].possible_values)
-    print("Value and possible_values", board.rows[3].row_cells[2].value, board.rows[3].row_cells[2].possible_values)
-    print("Value and possible_values", board.rows[3].row_cells[3].value, board.rows[3].row_cells[3].possible_values)
-    print(board.rows[2].get_possible_values_row())
-    print(board.columns[2].get_possible_values_column())
+    print("Possible values for this row",board.rows[0].get_possible_values_row())
+    print("Possible values for this row",board.rows[1].get_possible_values_row())
+    print("Possible values for this row",board.rows[2].get_possible_values_row())
+    print("Possible values for this row",board.rows[3].get_possible_values_row())
+    print("Possible values for this column",board.columns[0].get_possible_values_column())
+    print("Possible values for this column",board.columns[1].get_possible_values_column())
+    print("Possible values for this column",board.columns[2].get_possible_values_column())
+    print("Possible values for this column",board.columns[3].get_possible_values_column())
