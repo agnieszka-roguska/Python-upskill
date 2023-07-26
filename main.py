67e4835ab482d265cbfbaced92b9113a5303049e
Roguska <agnieszka.roguska@capgemini.com>
2023-07-26 17:01:51 +0200

    26.07 version filling both diagonals

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class Board:
-    board_size = 4
+    board_size = 9
     def __init__(self):
         self.rows = [Row() for row_number in range(self.board_size)]
         self.columns = [Column() for column_number in range(self.board_size)]
@@ -26,6 +26,51 @@ class Board:
         col_no = cell_no % self.board_size 
         square_no = row_no // square_size * square_size + col_no // square_size 
         return row_no, col_no, square_no
+    
+    def fill_random_cell(self): #randomly chooses one square and a random cell within that square; checks if this cell already has any value assigned and if not, assigns random value from its possible values
+        import random
+        if self.if_whole_board_filled is not True:
+            while True:
+                random_index = random.randint(0, len(self.cells) - 1)
+                random_cell = self.cells[random_index]
+                if len(random_cell.possible_values):
+                    print("all set")
+                    break
+            try:
+                random_value_to_be_set = random.choice(list(random_cell.possible_values))
+                print(random_value_to_be_set)
+                random_cell.set_value(random_value_to_be_set)
+            except:
+                print("Exception, could not set randomly choosen value for this position", random_index, random_cell.value, random_cell.possible_values)
+    
+    """def search_squares(self):
+        import random
+        if not self.if_whole_board_filled:
+            while True:
+                random_index = random.randint(0, len(self.squares) - 1)
+                random_square = self.squares[random_index]
+                if not random_square.if_whole_square_filled:
+                    for cell in random_square.square_cells:
+                        if len(cell.possible_values) == 1:
+                            cell.set_value(list(cell.possible_values)[0])"""
+    
+    def fill_diagonal(self):
+        import random
+        for row_index, row in enumerate(self.rows):
+            random_value_to_be_set = random.choice(list(row.row_cells[row_index].possible_values))
+            row.row_cells[row_index].set_value(random_value_to_be_set)
+            cell = row.row_cells[self.board_size - row_index - 1]
+            if cell.possible_values:
+                random_value_to_be_set = random.choice(list(cell.possible_values))
+                cell.set_value(random_value_to_be_set)
+            else:
+                print("Error, possible_values: ", cell.possible_values)
+
+    def if_whole_board_filled(self):
+        for cell in self.cells:
+            if cell.value == None:
+                return False
+        return True
 
 
 class Row():
@@ -47,7 +92,7 @@ class Row():
         self.row_cells.append(cell)
         cell.row = self
     
-    def erase(self, value): #what is this method doing?
+    def erase(self, value):
         for cell in self.row_cells:
             cell.erase(value)
 
@@ -84,7 +129,6 @@ class Square():
         possible_values = set()
         for cell in self.square_cells:
             possible_values = possible_values.union(cell.possible_values)
-            #print(cell.possible_values)
         return possible_values
     
     def append(self, cell):
@@ -94,6 +138,12 @@ class Square():
     def erase(self, value):
         for cell in self.square_cells:
             cell.erase(value)
+    
+    def if_whole_square_filled(self):
+        for cell in self.square_cells:
+            if cell.value == None:
+                return False
+        return True
 
 class Cell():
     def __init__(self, value_space):
@@ -107,7 +157,7 @@ class Cell():
         if value in self.possible_values:
             self.value = value
             self.possible_values = set()
-            self.row.erase(value) #what is this part of code doing?
+            self.row.erase(value)
             self.column.erase(value)
             self.square.erase(value)
         else:
@@ -117,25 +167,7 @@ class Cell():
         self.possible_values.discard(value)
 
 
-if __name__=="__main__":    
+if __name__=="__main__":
     board = Board()
-    board.cells[0].set_value(1)
-    board.cells[10].set_value(2)
-    board.cells[10].set_value(3)
-    board.cells[15].set_value(3)
+    board.fill_diagonal()
     board.print_rows()
-    print("Rows")
-    print("Possible values for this row",board.rows[0].get_possible_values())
-    print("Possible values for this row",board.rows[1].get_possible_values())
-    print("Possible values for this row",board.rows[2].get_possible_values())
-    print("Possible values for this row",board.rows[3].get_possible_values())
-    print("Columns")
-    print("Possible values for this column",board.columns[0].get_possible_values())
-    print("Possible values for this column",board.columns[1].get_possible_values())
-    print("Possible values for this column",board.columns[2].get_possible_values())
-    print("Possible values for this column",board.columns[3].get_possible_values())
-    print("Squares")
-    print("Possible values for this square",board.squares[0].get_possible_values())
-    print("Possible values for this square",board.squares[1].get_possible_values())
-    print("Possible values for this square",board.squares[2].get_possible_values())
-    print("Possible values for this square",board.squares[3].get_possible_values())
